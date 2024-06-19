f8ae15f7e907a5e31db893fa27a5d2faa8979d1f
asdfqweasd <229463581@qq.com>
2024-06-19 16:24:20 +1000

    pospal version

--- src/components/training/trainingPage.tsx
+++ src/components/training/trainingPage.tsx
@@ -1,46 +1,99 @@
-// pages/training.js
-import { useState } from 'react';
+// pages/training.tsx
+import { useState, useRef } from "react";
+import { pos_AndroidLink } from "@/lib/data";
 
 export default function Training() {
-  const [language, setLanguage] = useState('zh');
+  const [language, setLanguage] = useState("zh");
+  const videoRefs = useRef<(HTMLDivElement | null)[]>([]);
 
-  const handleLanguageChange = (lang:string) => {
+  const handleLanguageChange = (lang: string) => {
     setLanguage(lang);
   };
 
-   // YouTube 播放列表 ID，根据语言选择不同的播放列表
-   const playlistId = language === 'zh' ? 'PLARAy0Gh8lsbpj24ZXkit1nvr_k2tjoPK' : 'PLARAy0Gh8lsbpj24ZXkit1nvr_k2tjoPK';
+  const handleScrollToVideo = (index: number) => {
+    const videoElement = videoRefs.current[index];
+    if (videoElement) {
+      const elementRect = videoElement.getBoundingClientRect();
+      const absoluteElementTop = elementRect.top + window.scrollY;
+      const middle =
+        absoluteElementTop - (window.innerHeight / 2 - elementRect.height / 2);
+      window.scrollTo({ top: middle, behavior: "smooth" });
+    }
+  };
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
-    <div>
-      <h1 className='left-1/2 font-bold text-2xl'>Training Page</h1>
-      <div className=''>
-        <button className='mx-4  bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={() => handleLanguageChange('zh')}>中文</button>
-        <button className='mx-4  bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={() => handleLanguageChange('en')}>English</button>
+    <div className="flex">
+      <div className="flex-1 flex flex-col items-center">
+        <h1 className="font-bold text-2xl my-4">Training Page</h1>
+        <div className="my-4">
+          <button
+            className="mx-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            onClick={() => handleLanguageChange("zh")}
+          >
+            中文
+          </button>
+          <button
+            className="mx-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            onClick={() => handleLanguageChange("en")}
+          >
+            English
+          </button>
+        </div>
+
+        <div className="video-container my-5 w-full flex flex-col items-center">
+          {pos_AndroidLink[language].map((video, index) => (
+            <div
+              key={index}
+              ref={(el) => {
+                videoRefs.current[index] = el;
+              }}
+              className="mb-5"
+            >
+              <h2 className="text-xl font-semibold text-center">
+                {video.name}
+              </h2>
+              <iframe
+                width="660"
+                height="450"
+                src={`https://www.youtube.com/embed/${video.value}&vq=hd1080`}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        {/* 根据语言状态显示不同的内容 */}
-        {language === 'zh' && (
-          <p>中文组件</p>
-        )}
-        {language === 'en' && (
-          <p>English Component</p>
-        )}
+      <div className="ml-10 w-40">
+        <div className="fixed top-20 text-left">
+          <h2 className="font-bold text-xl">
+            {language === "zh" ? "导航" : "Navigation"}
+          </h2>
+          <ul className=" list-item">
+            {pos_AndroidLink[language].map((video, index) => (
+              <li key={index} className="my-1 ">
+                <button
+                  className="text-blue-500 hover:text-blue-700 truncate break-all text-left"
+                  onClick={() => handleScrollToVideo(index)}
+                >
+                  {video.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div className="text-left my-4">
+            <button
+              className="bg-blue-500 text-white font-semibold py-2 px-4 rounded"
+              onClick={handleScrollToTop}
+            >
+              {language === "zh" ? "回到顶端" : "Scroll to Top"}
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="video-container">
-        <iframe
-          width="860"
-          height="515"
-          src={`https://www.youtube.com/embed/videoseries?list=${playlistId}`}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <style jsx>{`
-        .video-container {
-          margin-top: 20px;
-        }
-      `}</style>
     </div>
   );
 }
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -39,3 +39,58 @@ export const categories = [
   { name: "Sports", value: "sports" },
   { name: "General", value: "general" },
 ];
+
+
+export const pos_AndroidLink ={ zh:[
+  { name: "前台登入收银系统", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=2" },
+  { name: "收银端交接班", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=1" },
+  { name: "重打收银小票", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=3" },
+  { name: "收银端做退款", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=4" },
+  { name: "收银端为商品做售罄", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=5" },
+  { name: "收银端恢复库存", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=6" },
+  { name: "收银端价格调整", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=7" },
+  { name: "收银端挂桌到桌号一般用于后支付，以及电话订单", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=8" },
+  { name: "收银端挂单完成支付", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=9" },
+  { name: "收银端收钱，一般用于外卖", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=10" },
+  { name: "收银端口味选择", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=11" },
+  { name: "收银端同步数据，后台有很多更新需要此操作", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=12" },
+  { name: "收银端修改商品名称", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=13" },
+  { name: "收银端打开钱箱", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=14" },
+  { name: "在收银端使用暂存挂单", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=15" },
+  { name: "在收银端添加厨打IP或KDS IP", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=16" },
+  { name: "在收银端查看主机网络IP地址（网线）", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=17" },
+  { name: "在收银端查看主机IP地址（Wi-Fi）", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=18" },
+  { name: "在副收银上设置主机IP地址", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=19" },
+  { name: "在收银端设置商品厨房打印机", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=20" },
+  { name: "在收银端添加新的商品", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=21" },
+  { name: "在收银端设置商品厨房打印机", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=22" },
+  { name: "在收银端拆单", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=23" }
+],
+  en:[
+    { name: "Login to POSPAL System", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=2" },
+    { name: "Handover on POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=1" },
+    { name: "Reprint Tax Invoice", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=3" },
+    { name: "Refund on POSPAL end", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=4" },
+    { name: "Set sold out item on POS end", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=5" },
+    { name: "Restore Stock on POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=6" },
+    { name: "Change or Increase Price on POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=7" },
+    { name: "Hold order under Table No,Normally Using for Pay after or Phone Takeaway", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=8" },
+    { name: "Complete Order under Table No.", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=9" },
+    { name: "Order and Go on POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=10" },
+    { name: "Choose Item Flavour on POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=11" },
+    { name: "Data synchronization on POS when did lots of change on Back-end", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=12" },
+    { name: "Change Item names on POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=13" },
+    { name: "Open Cash Drawer on POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=14" },
+    { name: "Temporary Holding Order on POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=15" },
+    { name: "Bind Kitchen IP address or KDS IP address on POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=16" },
+    { name: "Check Ethernet IP Address on Main POS Machine", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=17" },
+    { name: "Check Wi-Fi IP address on Main POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=18" },
+    { name: "Add Main POS IP Address on Secondary POS", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=19" },
+    { name: "Bind Item to specified Kitchen Printer on POS end", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=20" },
+    { name: "Add new Item on POS end", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=21" },
+    { name: "Items connect to Kitchen Printer", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=22" },
+    { name: "Split Bill", value: "playlist?list=PLARAy0Gh8lsbqO01WbNTGylCQd3oOJtoe&si=bRzfMbDVlUI7E4cr&index=23" },
+  ]
+}
+
+
