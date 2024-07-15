c71b9d4b5653136697e138696e0bf9a0df65a7a3
asdfqweasd <229463581@qq.com>
2024-07-15 16:56:31 +1000

    fix home text

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <section
       id="home"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      className=" px-16 max-w-[50rem] mx-auto text-center sm:mb-0 scroll-mt-[100rem] flex justify-center items-center"
     >
       <div className="text-2xl">Welcome to Intelligeneer&apos;s training dashboard</div>
     </section>
